665f55236f21a04c4443f0e8ddcd38c2d8f21de6
Connor Dinnadge <36665474+connordinnadge@users.noreply.github.com>
2024-06-27 10:23:05 +0100

    added empty paint calc comp

---
+++ src/components/PaintCalculator/index.tsx
@@ -0,0 +1,11 @@
+import type { NextComponentType, NextPageContext } from 'next'
+
+const Index: NextComponentType<NextPageContext> = () => {
+  return (
+    <div>
+        <p>Calculator will go here...</p>
+    </div>
+  )
+}
+
+export default Index
