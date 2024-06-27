65c23ec75e2cd1b0be10fcbaae43b45f00b34f26
Connor Dinnadge <36665474+connordinnadge@users.noreply.github.com>
2024-06-27 10:41:24 +0100

    added background image to section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,23 @@
-import PaintCalculator from "@/components/PaintCalculator";
+import PaintCalculator from '@/components/PaintCalculator'
+import Image from 'next/image'
+import bgImage from '../../public/images/paint-pour.jpg'
 
 export default function HomePage() {
-  return (
-    <main>
-      <section className="px-3 py-5 h-[100vh] w-full bg-orange-200">
-        <h1>Acrylic Pour Pain Calculator</h1>
-        <p>Some words about it</p>
-        <PaintCalculator />
-      </section>
-    </main>
-  );
+    return (
+        <main>
+            <section className='relative flex h-[100vh] w-full flex-col items-center justify-center px-3 py-5 text-center'>
+                <Image
+                    src={bgImage}
+                    alt='Acrylic Pour Paint Calculator'
+                    layout='fill'
+                    objectFit='cover'
+                    className='z-0'
+                    priority={true}
+                />
+                <h1>Acrylic Pour Pain Calculator</h1>
+                <p>Some words about it</p>
+                <PaintCalculator />
+            </section>
+        </main>
+    )
 }
