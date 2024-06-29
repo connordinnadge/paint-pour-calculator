8ced05158ab14f806bc0e297f2330b14fc716dcb
Connor Dinnadge <36665474+connordinnadge@users.noreply.github.com>
2024-06-29 21:50:15 +0100

    added plausible provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,12 @@
 import '@/styles/globals.css'
 
 import { GeistSans } from 'geist/font/sans'
+import PlausibleProvider from 'next-plausible'
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang='en' className={`${GeistSans.variable}`}>
+            <PlausibleProvider domain='paint-calculator.connordinnadge.com' />
             <body>{children}</body>
         </html>
     )
