c071c7a99b4d3825ab61d8ac3658bd9018cb1a2c
Connor Dinnadge <36665474+connordinnadge@users.noreply.github.com>
2024-06-29 21:42:42 +0100

    added updated page metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,76 @@ import PaintCalculator from '@/components/PaintCalculator'
 import Image from 'next/image'
 import bgImage from '../../public/images/paint-pour.jpg'
 import { Card, CardContent, CardHeader } from '@/components/ui/card'
+import type { Metadata } from 'next'
+
+const title = 'Acrylic Pour Paint Calculator | Connor Dinnadge'
+const description =
+    'Calculate the exact paint needed for your acrylic pour projects. Choose your canvas shape and size, and get precise paint volume recommendations for perfect consistency. Ideal for artists seeking flawless results.'
+const ogImage = '/images/meta_image.jpg'
+const twitterImage = '/images/meta_image.jpg'
+
+export const metadata: Metadata = {
+    title: title,
+    description: description,
+    keywords: [
+        'acrylic pour paint calculator',
+        'paint pouring calculator',
+        'acrylic pouring mix calculator',
+        'acrylic pour calculator',
+        'pour painting mix calculator',
+        'acrylic paint pouring ratios',
+        'acrylic pour paint mix ratios',
+        'paint pouring consistency calculator',
+        'acrylic pour paint measurements',
+        'pour painting measurements',
+        'acrylic pouring medium calculator',
+        'paint pouring formula',
+        'acrylic pour paint guide',
+        'acrylic pour paint tips',
+        'acrylic pouring techniques',
+        'acrylic paint mixing calculator',
+        'acrylic pour painting tips',
+        'pour paint consistency guide',
+        'acrylic pour paint mix guide',
+        'paint pouring supplies',
+    ],
+    openGraph: {
+        title: title,
+        description: description,
+        url: 'https://www.paint-calculator.connordinnadge.com',
+        siteName: 'Acrylic Pour Paint Calculator',
+        images: [
+            {
+                url: ogImage,
+                width: 600,
+                height: 400,
+            },
+        ],
+        locale: 'en_UK',
+        type: 'website',
+    },
+    twitter: {
+        card: 'summary_large_image',
+        title: title,
+        description: description,
+        creator: '@connordinnadge',
+        images: [
+            {
+                url: twitterImage,
+                width: 600,
+                height: 400,
+            },
+        ],
+    },
+    robots: {
+        index: true,
+        follow: true,
+        googleBot: {
+            index: true,
+            follow: true,
+        },
+    },
+}
 
 export default function HomePage() {
     return (
